ee6b6dd16fa6c4a6390f8b7a535ac98ec26a07a3
Benjaexplot2227 <ben.andres.gonzalez.2227@gmail.com>
2025-12-28 17:27:17 -0300

    Add files via upload

---
+++ app/src/main/java/com/equipo31/app/Controller/aboutit.java
@@ -0,0 +1,15 @@
+package com.equipo31.app.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class aboutit {
+
+    //Renderiza la plantilla index del templates
+    @GetMapping("/acerca")
+    public String mostrarPagina() {
+        return "aboutit"; 
+    }
+
+}
