64a8f3a386e7d594b2995727d9d9116710de395d
Benjaexplot2227 <ben.andres.gonzalez.2227@gmail.com>
2026-01-04 18:24:16 -0300

    Delete app directory

--- app/src/main/java/com/equipo31/app/repository/SentimentRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.equipo31.app.repository;
-
-import com.equipo31.app.entity.SentimentRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SentimentRecordRepository extends JpaRepository<SentimentRecord, Long> {
-    long countByPrevision(String prevision);
-}
