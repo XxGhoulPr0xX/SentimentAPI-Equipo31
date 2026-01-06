24c91ab4a16a264fcb327ca03e3f7eef3d3f29ce
XxGhoulPr0xX <javieralvaradomarquez1102@gmail.com>
2026-01-05 19:12:10 -0600

    Devuelve en formato json la base de datos h2, lista para consumirse

--- app/src/main/java/com/equipo31/app/Controller/StatsController.java
+++ app/src/main/java/com/equipo31/app/Controller/StatsController.java
@@ -1,25 +1,31 @@
 package com.equipo31.app.Controller;
 
-import com.equipo31.app.dto.StatsResponse;
+import com.equipo31.app.entity.SentimentRecord;
 import com.equipo31.app.repository.SentimentRecordRepository;
+import com.equipo31.app.Service.EstadisticasRegistros;
+
+import java.util.List; 
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/stats")
 public class StatsController {
 
     private final SentimentRecordRepository repo;
+    private final EstadisticasRegistros estadisticasService;
 
-    public StatsController(SentimentRecordRepository repo) {
+    public StatsController(SentimentRecordRepository repo, EstadisticasRegistros estadisticasService) {
         this.repo = repo;
+        this.estadisticasService = estadisticasService;
     }
-
-    @GetMapping("stats")
-    public StatsResponse stats() {
-        long total = repo.count();
-        long positivos = repo.countByPrevision("Positivo");
-        long negativos = repo.countByPrevision("Negativo");
-        long neutros = repo.countByPrevision("Neutro");
-        return new StatsResponse(total, positivos, negativos, neutros);
-    }
+    
+    //Endpoint que se encarga de enviar el formato json de la base de datos h2, contiene "ID", "FECHA", "SENTIMIENTO", "PROB", "TEXTO"
+    @GetMapping("/list")
+        public ResponseEntity<List<SentimentRecord>> imprimirRegistrosEnConsola() {
+            List<SentimentRecord> registros = repo.findAll();
+            estadisticasService.imprimirRegistrosEnConsola(registros);
+            return ResponseEntity.ok(registros);
+        }
 }
