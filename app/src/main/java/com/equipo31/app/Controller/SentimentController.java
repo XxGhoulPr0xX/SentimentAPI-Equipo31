d7e61631aa60ef49e298eec7412acd85bb9d76be
Augusto Paz <augustoignaciopaz@gmail.com>
2026-01-09 21:24:55 -0300

    Fix: Enable CORS for Render

--- app/src/main/java/com/equipo31/app/Controller/SentimentController.java
+++ app/src/main/java/com/equipo31/app/Controller/SentimentController.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/sentiment-api")
 public class SentimentController {
