ee1641f2d262c63acc28f47356b175ceb8ca371e
Augusto Paz <augustoignaciopaz@gmail.com>
2026-01-09 21:04:37 -0300

    Config: Set production API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: '/api'
+    apiUrl: 'https://sentimentapi-equipo31.onrender.com'
 };
 // Note: '/api' relative path allows us to use Nginx proxy pass without hardcoding the IP/Domain in the build.
 // This is much easier for hackathons than rebuilding every time the IP changes.
