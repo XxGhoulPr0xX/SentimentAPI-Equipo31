5ce9811e52f342d1658fcb5bec80dbe2c681d6f1
XxGhoulPr0xX <javieralvaradomarquez1102@gmail.com>
2026-01-13 11:57:50 -0600

    Comunicacion interna mediante nginx por docker compose

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://sentimentapi-equipo31.onrender.com'
+    apiUrl: '/api' // Cambiado de 'https://sentimentapi-equipo31.onrender.com' a '/api'
 };
-// Note: '/api' relative path allows us to use Nginx proxy pass without hardcoding the IP/Domain in the build.
-// This is much easier for hackathons than rebuilding every time the IP changes.
